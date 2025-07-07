af59513c98580f2358a46cb9162f3c10bc3df505
ME Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2025-07-07 11:28:19 -0700

    string functions

---
+++ echo17.php
@@ -0,0 +1,33 @@
+<?php
+    $username = "Bro Code";
+    $phone = "123-456-7890";
+
+    //$username = strtolower($username);
+    //$username = strtoupper($username);
+    //$username = trim($username);
+    //$username = str_pad($username, 20, "0");
+    //$phone = str_replace("-", "", $phone);
+    //$username = strrev($username);
+    //$username = str_shuffle($username);
+    //$equals = strcmp($username, "Bro Code");
+    //$count = strlen($phone);
+    //$index = strpos($phone, "-");
+    //$firstname = substr($username, 0, 3);
+    //$lastname = substr($username, 4);
+    
+    /*
+      $username = "Bro Code";
+      $fullname = explode(" ", $username);  // string to array.
+      foreach($fullname as $name){
+        echo $name . "<br>";
+      }
+    */
+    
+    /*
+       $username = array("Bro","The","Code");
+       $username = implode("-", $username);  // array to string.
+       echo $username;
+    */
+
+    echo $username;
+?>
